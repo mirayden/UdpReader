7578a11b9b13fa91bce8ff557c5dd067fcce9278
Andrey Mir <mir.andrey@gmail.com>
2019-09-16 12:27:44 +0200

    Initial commit. Added an example for the UDP cliend and server communication.

---
+++ src/Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Please input the messages you want to send to the server and press enter. Or press Ctrl + C to stop.");
+
+			while (true)
+			{
+				SendMessage(Console.ReadLine());
+				Console.WriteLine("Message was sent.");
+			}
+		}
+
+		static void SendMessage(string messageString)
+		{
+			var serverIp = IPAddress.Parse("127.0.0.1");
+			int serverPort = 5555;
+			var endpoint = new IPEndPoint(serverIp, serverPort);
+
+			byte[] message = Encoding.ASCII.GetBytes(messageString);
+			using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+			{
+				socket.SendTo(message, message.Length, SocketFlags.None, endpoint);
+			}
+		}
+	}
+}
---
+++ src/Server/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var serverIp = IPAddress.Parse("127.0.0.1"); 
+			int serverPort = 5555;
+
+			var endpoint = new IPEndPoint(serverIp, serverPort);
+			using (var listener = new UdpClient(serverPort))
+			{
+				Console.WriteLine("Server started. Waiting for calls.");
+
+				try
+				{
+					while (true)
+					{
+						byte[] requestBytes = listener.Receive(ref endpoint);
+
+						string requestMessage = Encoding.ASCII.GetString(requestBytes, 0, requestBytes.Length);
+						Console.WriteLine($"Incoming request from {endpoint}. Message: {requestMessage}");
+					}
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e.ToString());
+				}
+			}
+		}
+	}
+}
